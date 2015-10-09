799f615561a2c13043a8b43f0c4c5998f71a24a2
Rebekah Shein <rmshein@gmail.com>
2015-10-09 10:46:02 -0400

    fixed mongoose spelling

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express'),
-    PORT    = process.env.PORT || 5432,
-    server  = express(),
-    MONGOURI = process.env.MONGOLAB_URI,
-    dbname = "some_useful_name",
-    mongoose = require('monggose');
+var express   = require('express'),
+    PORT      = process.env.PORT || 5432,
+    server    = express(),
+    MONGOURI  = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
+    dbname    = "some_useful_name",
+    mongoose  = require('mongoose');
 
 server.get('/test', function (req, res) {
   res.write("Welcome to my amazing app");
