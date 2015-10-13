74eb69b06913c9604b5596af3c5372064f3e5c15
Rebekah Shein <rmshein@gmail.com>
2015-10-13 10:33:44 -0400

    created article schema in models dir

---
+++ models/article.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+
+// defined article schema
+var articleSchema = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    date: Date,
+    category: [ String ],
+    summary: { type: String, maxlength: 140 },
+    article: { type: String, required: true }
+});
+
+// article model
+var Article = mongoose.model('Article', articleSchema);
+
+// article model export
+module.exports = Article;
