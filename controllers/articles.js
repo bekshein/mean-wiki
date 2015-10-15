0df7c94062a2040dba650d5b283212769f3ca7e3
Rebekah Shein <rmshein@gmail.com>
2015-10-15 16:05:31 -0400

    required User model, updated routes to force user login and set author to session userName

--- controllers/articles.js
+++ controllers/articles.js
@@ -3,34 +3,58 @@
 var express = require('express'),
     router  = express.Router(),
     Article = require('../models/article');
+    User    = require('../models/user');
 
 // INDEX all articles stored in db
 router.get('/', function (req, res) {
-  Article.find({}, function (err, allArticles) {
-    if (err) {
-      res.redirect(302, '/welcome');
-    } else {
-      res.render('articles/index', {
-        articles: allArticles
-      });
-    }
-  });
+  if (!req.session.userId) {
+    req.session.flash.message = "You must be logged in to view page.";
+    res.redirect(302, '/users/login');
+  } else {
+    Article.find({}, function (err, allArticles) {
+      if (err) {
+        req.session.flash.message = "Error finding index...";
+      } else {
+        res.render('articles/index', {
+          articles: allArticles
+        });
+      }
+    });
+  }
 });
 
 // NEW article form
 router.get('/new', function (req, res) {
-  res.render('articles/new');
+  if (!req.session.userId) {
+    req.session.flash.message = "You must be logged in to view page.";
+    res.redirect(302, '/users/login');
+  } else {
+    User.findById(req.session.userId, function (err, user) {
+      if (err) {
+        req.session.flash.message = "An error has occurred...";
+        res.redirect(302, '/');
+      } else if (user) {
+        res.render('articles/new', {
+          user: user
+        });
+      }
+    });
+  }
 });
 
 // CREATE new article and redirects
 router.post('/', function (req, res) {
-  var newArticle = new Article(req.body.article);
+  var articleOptions = req.body.article;
+  articleOptions.author = req.session.userName;
+
+  var newArticle = new Article(articleOptions);
 
   newArticle.save(function (err, article) {
     if (err) {
-      console.log(err);
+      req.session.flash.message = "Could not save article to db...";
       res.redirect(302, '/articles/new');
     } else {
+      req.session.flash.message = "Article saved to db...";
       res.redirect(302, '/articles');
     }
   });
@@ -38,46 +62,63 @@ router.post('/', function (req, res) {
 
 // SHOW article by id
 router.get('/:id', function (req, res) {
-  var articleID = req.params.id;
+  if (!req.session.userId) {
+    req.session.flash.message = "You must be logged in to view page.";
+    res.redirect(302, '/users/login');
+  } else {
+    var articleID = req.params.id;
 
-  Article.findById(articleID, function (err, specificArticle) {
-    if (err) {
-      res.redirect(302, '/articles/index');
-    } else {
-      res.render('articles/show', {
-        article: specificArticle
-      });
-    }
-  });
+    Article.findById(articleID, function (err, specificArticle) {
+      if (err) {
+        req.session.flash.message = "Error finding Article...";
+        res.redirect(302, '/articles');
+      } else {
+        res.render('articles/show', {
+          article: specificArticle
+        });
+      }
+    });
+  }
 });
 
 // DELETE article and redirects
 router.delete('/:id', function (req, res) {
-  var articleID = req.params.id;
+  if (!req.session.userId) {
+    req.session.flash.message = "You must be logged in to view page...";
+    res.redirect(302, '/users/login');
+  } else {
+    var articleID = req.params.id;
 
-  Article.findByIdAndRemove({ _id: articleID }, function (err) {
-    if (err) {
-      res.redirect(302, '/articles/show');
-    } else {
-      res.redirect(302, '/articles');
-    }
-  });
+    Article.findByIdAndRemove(articleID, function (err) {
+      if (err) {
+        req.session.flash.message = "Error with deleting...";
+      } else {
+        req.session.flash.message = "Succesfully deleted...";
+        res.redirect(302, '/articles');
+      }
+    });
+  }
 });
 
 // EDIT article by id form
 router.get('/:id/edit', function (req, res) {
-  var articleID = req.params.id;
+  if (!req.session.userId) {
+    req.session.flash.message = "You must be logged in to view page.";
+    res.redirect(302, '/users/login');
+  } else {
+    var articleID = req.params.id;
 
-  Article.findById({ _id: articleID }, function (err, specificArticle) {
-    if (err) {
-      res.write("Article Update is Bad");
-      res.end();
-    } else {
-      res.render('articles/edit', {
-        article: specificArticle
-      });
-    }
-  });
+    Article.findById(articleID, function (err, specificArticle) {
+      if (err) {
+        req.session.flash.message = "Error finding Article...";
+        res.redirect(302, '/articles');
+      } else {
+        res.render('articles/edit', {
+          article: specificArticle
+        });
+      }
+    });
+  }
 });
 
 // UPDATE article and redirects
@@ -85,10 +126,13 @@ router.patch('/:id', function (req, res) {
   var articleID = req.params.id;
   var articleParams = req.body.article;
 
-  Article.findByIdAndUpdate({ _id: articleID }, articleParams, function (err, updatedArticle) {
+  articleParams.author = req.session.userName;
+
+  Article.findByIdAndUpdate(articleID, articleParams, function (err, updatedArticle) {
     if (err) {
-      res.redirect(302, '/articles/edit');
+      req.session.flash.message = "Update is Bad...";
     } else {
+      req.session.flash.message = "Update completed...";
       res.redirect(302, "/articles/" + updatedArticle._id);
     }
   });
